76a3799d1ea420ad5ffe2221bef508a92df976bc
Bruno Carvalho <brunocarvalho9810@gmail.com>
2021-09-07 19:07:17 -0300

    Primeira configuracao

---
+++ src/main/java/com/juntospormais/JuntosPorMaisApplication.java
@@ -0,0 +1,13 @@
+package com.juntospormais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuntosPorMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuntosPorMaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juntospormais/JuntosPorMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juntospormais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuntosPorMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
